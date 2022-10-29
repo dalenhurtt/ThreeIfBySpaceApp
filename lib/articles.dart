5a4277390e2d17cf9aab5bbc8b07bf790cdd6ac1
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2022-10-28 23:07:15 -0700

    make articles class asynchronous

--- lib/articles.dart
+++ lib/articles.dart
@@ -7,19 +7,26 @@ class Articles {
   // Attributes of the objects: https://developer.wordpress.org/rest-api/reference/posts/
 
   // This variable will be used in the future for implementing caching
-  static dynamic _posts;
+  static List<Post>? _posts;
 
-  static dynamic getPosts() async {
-    final WPResponse res = await _api.posts.fetch();
-    _posts ??= res.data;
+  static Future<List<Post>?> getPosts() async {
+    if (_posts == null) await _updatePosts();
     return _posts;
   }
 
+  // Fetches posts from the wordpress app. This should be used with a cache
+  // implementation in the future
+  static Future<void> _updatePosts() async {
+    final WPResponse res = await _api.posts.fetch();
+    _posts = res.data;
+  }
+
   // Find post by ID
-  static dynamic getPostByID(int id) {
-    final post = _posts.firstWhere((element) => element.id == id, orElse: () {
-      return null;
-    });
+  static Future<Post?> getPostByID(int? id) async {
+    if (_posts == null) await _updatePosts();
+
+    Post? post = _posts?.firstWhere((element) => element.id == id);
+
     return post;
   }
 }
