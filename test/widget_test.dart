c3d4ada3d707e9eae8ba73af1cb62e58e0f2d065
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2022-11-02 02:34:39 -0700

    fix emulator running errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:threeifbyspace/main.dart';
+import 'package:threeifbyspaceapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
