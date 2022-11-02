672e9052c7d2ac3643902e13d1aca88cced5ccf7
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2022-11-02 02:43:24 -0700

    fix ios errors - it created duplicate file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:threeifbyspaceapp/main.dart';
+import 'package:threeifbyspace/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
